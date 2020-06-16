c4883d47ee88d8a23519d05a0101619d2884b259
Jon Husen <jon.husen@gmail.com>
2020-06-16 13:17:21 -0400

    Second attempt using a dict comprehension

--- python_morsels/count.py
+++ python_morsels/count.py
@@ -1,9 +1,11 @@
 def count_words(phrase):
     word_list = [word.strip(",;.!?").casefold() for word in phrase.split()]
-    word_count = {}
-    for word in word_list:
-        if word in word_count.keys():
-            word_count[word] = word_count[word] + 1
-        else:
-            word_count.update({word: 1})
+    word_count = {word: word_list.count(word) for word in word_list}
+
+    # word_count = {}
+    # for word in word_list:
+    #     if word in word_count.keys():
+    #         word_count[word] = word_count[word] + 1
+    #     else:
+    #         word_count.update({word: 1})
     return word_count
