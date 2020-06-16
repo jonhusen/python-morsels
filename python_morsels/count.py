21745e451c33495e310afb2646092b8a99abfae3
Jon Husen <jon.husen@gmail.com>
2020-06-16 14:06:51 -0400

    Added PM instructions

--- python_morsels/count.py
+++ python_morsels/count.py
@@ -1,3 +1,26 @@
+"""
+I want you to write a function that accepts a string and returns a mapping (a dictionary or dictionary-like structure) that has words as the keys and the number of times each word was seen as the values.
+
+Your function should work like this:
+
+>>> count_words("oh what a day what a lovely day")
+{'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
+>>> count_words("don't stop believing")
+{"don't": 1, 'stop': 1, 'believing': 1}
+Bonus 1
+
+As a bonus, make sure your function works well with mixed case words:
+
+>>> count_words("Oh what a day what a lovely day")
+{'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
+Bonus 2
+
+For even more of a bonus try to get your function to ignore punctuation outside of words:
+
+>>> count_words("Oh what a day, what a lovely day!")
+{'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
+"""
+
 import re
 
 
