75c753d0a17a6245d3c117e11c15229f9b3ab453
Jon Husen <jon.husen@gmail.com>
2020-06-16 13:16:33 -0400

    First attempt

---
+++ python_morsels/count.py
@@ -0,0 +1,9 @@
+def count_words(phrase):
+    word_list = [word.strip(",;.!?").casefold() for word in phrase.split()]
+    word_count = {}
+    for word in word_list:
+        if word in word_count.keys():
+            word_count[word] = word_count[word] + 1
+        else:
+            word_count.update({word: 1})
+    return word_count
