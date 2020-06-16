fdb58ff58ecd35a80b7c71e236f09cab5cc6109b
Jon Husen <jon.husen@gmail.com>
2020-06-16 13:58:45 -0400

    more accurate regex

--- python_morsels/count.py
+++ python_morsels/count.py
@@ -2,7 +2,7 @@ import re
 
 
 def count_words(phrase):
-    word_list = [word.casefold() for word in re.findall("[A-Za-z']+", phrase)]
+    word_list = [word.casefold() for word in re.findall(r"\b[\w'-]+\b", phrase)]
     return {word: word_list.count(word) for word in word_list}
 
     # word_count = {}
